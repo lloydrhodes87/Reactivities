2ccf5287054e138c7ccae118ac657182766374a4
Ingrid Alcalde <ingridalcalde@Ingrids-MacBook-Pro-2.local>
2019-10-11 18:57:07 +0100

    crud dotnet app built

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application.Activities;
@@ -7,21 +8,38 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class ActivitiesController
     {
-        [Route("api/[controller]")]
-        [ApiController]
+        private readonly IMediator _mediatr;
 
-        public class ValuesController : ControllerBase
+        public ActivitiesController(IMediator mediatr) => _mediatr = mediatr;
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> List() => await _mediatr.Send(new List.Query());
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> Details(Guid id) => await _mediatr.Send(new Details.Query{Id = id});
+
+        [HttpPost]
+
+        public async Task<ActionResult<Unit>> Create(Create.Command command) => await _mediatr.Send(command);
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command) 
         {
-
-            private readonly IMediator _mediatr;
-
-            public ValuesController(IMediator mediatr) => _mediatr = mediatr;
-
-            [HttpGet]
-            public async Task<ActionResult<List<Activity>>> List() => await _mediatr.Send(new List.Query());
+            command.Id = id;
+            return await _mediatr.Send(command);
         }
-    }
 
+        [HttpDelete("{id}")]
+
+        public async Task<ActionResult<Unit>> Delete(Guid id)
+        {
+            return await _mediatr.Send(new Delete.Command{Id = id});
+        }   
+    }
 }
+
